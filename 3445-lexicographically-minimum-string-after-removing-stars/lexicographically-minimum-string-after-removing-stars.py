e7c8fa8acc765ef44266430a615c810cb770882a
vaish621 <122389160+vaish621@users.noreply.github.com>
2025-06-26 23:22:23 +0530

    Time: 1239 ms (33.55%) | Memory: 19.1 MB (70.33%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.py
@@ -1,22 +1,29 @@
-from collections import defaultdict, OrderedDict
+class Solution(object):
+    def clearStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
 
-class Solution:
-    def clearStars(self, s: str) -> str:
-        d = defaultdict(list)
-        to_remove = set()
+        d=defaultdict(list)
+        re=set()
 
-        for i, ch in enumerate(s):
-            if ch != '*':
+        for i,ch in enumerate(s):
+            if ch!='*':
                 d[ch].append(i)
             else:
+                #print(d)
                 for ch in sorted(d.keys()):
                     if d[ch]:
-                        to_remove.add(d[ch].pop())
+                        re.add(d[ch].pop())
                         break
+        
+        ans=''
 
-        res = []
-        for i, ch in enumerate(s):
-            if i not in to_remove and ch != '*':
-                res.append(ch)
+        for i,ch in enumerate(s):
+            if ch!='*' and i not in re:
+                ans+=ch
+        
+        return ans
 
-        return ''.join(res)
+        
