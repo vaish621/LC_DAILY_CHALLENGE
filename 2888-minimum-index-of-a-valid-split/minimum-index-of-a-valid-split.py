a49f036a6764bc41c2b06f6f591f34278a8e6f69
vaish621 <122389160+vaish621@users.noreply.github.com>
2025-09-30 13:12:50 +0530

    Time: 110 ms (25.39%) | Memory: 34.8 MB (10.22%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -1,39 +1,24 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
-        fre=0
-        num=0
 
-        for i in nums:
-            if fre==0:
-                fre=1
-                num=i
-            elif i==num:
-                fre+=1
-            else:
-                fre-=1
-                if fre==0:
-                    fre=1
-                    num=i
-        
-        if nums.count(num) * 2 <= len(nums):
-            return -1 
-        
-        pre=[0]*len(nums)
+        c=collections.Counter(nums)
+        n=len(nums)
 
-        for i in range(len(nums)):
-            if nums[i]==num:
-                pre[i]=pre[i-1]+1
-            else:
-                pre[i]=pre[i-1]
-        
-        
         i=0
 
-        while i<len(nums)-1:
-            le=pre[i]
-            re=pre[-1]-pre[i]
-            if le*2>(i+1) and re*2>(len(nums)-i-1):
+        m1=defaultdict(int)
+
+        while i<len(nums):
+            m1[nums[i]]+=1
+            c[nums[i]]-=1
+
+            if m1[nums[i]]>(i+1)//2 and c[nums[i]]>(n-i-1)//2:
                 return i
+            
             i+=1
+        
+
         return -1
+            
+
         
