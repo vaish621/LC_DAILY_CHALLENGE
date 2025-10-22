33456f24e260e4ad26404b1849d2fa6e3de70e12
vaish621 <122389160+vaish621@users.noreply.github.com>
2025-10-22 21:11:19 +0530

    Time: 198 ms (54.99%) | Memory: 30 MB (56.50%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
@@ -1,15 +1,34 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
+        maxi=[0]*len(nums)
+        maxi[-1]=nums[-1]
 
-        ma_a=float('-inf')
-        ma_ab=float('-inf')
-        ma_abc=0
-
-        for i in nums:
-            ma_abc=max(ma_abc,ma_ab*i)
-            ma_ab=max(ma_ab,ma_a-i)
-            ma_a=max(ma_a,i)
+        for i in range(len(nums)-2,-1,-1):
+            maxi[i]=max(nums[i],maxi[i+1])
         
-        return ma_abc
+        minind=-1
+        maxind=-1
+        mi=float("inf")
+        ma=float("-inf")
+        ans=0
+
+        for i in range(len(nums)):
+            if nums[i]>ma:
+                ma=nums[i]
+                maxind=i
+            
+            if nums[i]<mi:
+                mi=nums[i]
+                minind=i
+            
+            if minind<maxind:
+                mi=float("inf")
+                minind=-1
+            elif minind!=-1 and maxind!=-1 and maxind<minind:
+                if i+1<len(nums):
+                    ans=max(ans,(ma-mi)*maxi[i+1])
+        
+        return ans
+
 
         
