7f6f15b0b8d15e7e72793bd730fab1526bcc5aae
vaish621 <122389160+vaish621@users.noreply.github.com>
2025-11-01 18:37:41 +0530

    Time: 2317 ms (12.19%) | Memory: 109.8 MB (23.46%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
@@ -1,39 +1,42 @@
-from collections import defaultdict
-
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        for u, v in edges:
+
+        adj=defaultdict(list)
+
+        for u,v in edges:
             adj[u].append(v)
 
-        n = len(colors)
-        visit = [0] * n   # 0 = unvisited, 1 = visiting, 2 = done
-        dp = [[0] * 26 for _ in range(n)]
-        self.cycle = False
+        dp=[[0]*26 for _ in range(len(colors))]
+        self.cycle=False
+
+        visit=[0]*len(colors)
 
         def dfs(i):
-            if visit[i] == 1:  # cycle found
-                self.cycle = True
-                return [0] * 26
-            if visit[i] == 2:  # already computed
+            if visit[i]==1:
+                self.cycle=True
+                return [0]*26
+            
+            if visit[i]==2:
                 return dp[i]
+            
+            visit[i]=1
 
-            visit[i] = 1
-            for nei in adj[i]:
-                res = dfs(nei)
+            for j in adj[i]:
+                res=dfs(j)
                 for c in range(26):
-                    dp[i][c] = max(dp[i][c], res[c])
-
-            dp[i][ord(colors[i]) - ord('a')] += 1
-            visit[i] = 2
+                    dp[i][c]=max(dp[i][c],res[c])
+            
+            visit[i]=2
+            dp[i][ord(colors[i])-ord('a')]+=1
             return dp[i]
 
-        ans = -1
-        for i in range(n):
-            if visit[i] == 0:
+        ans=0
+        for i in range(len(colors)):
+            if visit[i]==0:
                 dfs(i)
-            ans = max(ans, max(dp[i]))
-
+            ans=max(ans,max(dp[i]))
+        
         if self.cycle:
             return -1
         return ans
+        
